3e1e12fe1a7600236f17adfc99d69fbe656076bc
R4F405 <sanpablorafa@gmail.com>
2025-01-14 23:14:47 +0100

    Creación de animaciones: IDLE y RUN
    Se ha creado tres animaciones para el personaje, la primera es un movimiento suave cuando el personaje esta quito (IDLE), la segunda es un movimiento mas pesado para cuando el personaje corre (RUN), y por ultimo se ha creado la animación que gira el personaje  en espejo según su movimiento X (Izquierda, derecha).

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 5f;
     Vector2 direction;
+    
 
     Rigidbody2D rigidBody;
     Animator animator;
@@ -18,7 +19,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate() 
     {
-        //rigidBody.velocity = direction * speed; 
+        //Crea el movimiento segun direccion y velocidad
         Vector2 targetPosition = rigidBody.position + direction * speed * Time.fixedDeltaTime; 
         rigidBody.MovePosition(targetPosition);
     }
@@ -33,13 +34,21 @@ public class PlayerMovement : MonoBehaviour
     //Funcion de movimientos
     private void Movement() 
     {
-        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized; //Obtiene el movimiento atraves de WASD
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized; //Obtiene la direccion del movimiento atraves de WASD o flechas
     }
 
     //Funcion de animaciones
     private void Animations() 
     {
+        //Si esta quito (IDLE)
+        if (direction == Vector2.zero) animator.SetBool("running", false);
+        //Si se mueve (RUN)
+        else animator.SetBool("running", true);
 
+        // Si se mueve a la serecha (ANIMACION)
+        if (direction.x > 0) transform.localScale = new Vector3(1f, 1f, 1f); 
+         // Si se mueve a la izquierda (ANIMACION INVERSA)
+        else if (direction.x < 0)  transform.localScale = new Vector3(-1f, 1f, 1f); 
     }
 
 }
