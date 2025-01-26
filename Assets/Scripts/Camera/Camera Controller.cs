e6a9b78d9891f64bef1666a3427d329056974ad1
R4F405 <sanpablorafa@gmail.com>
2025-01-26 03:05:55 +0100

    Definición tamaño background
    Se ha definido el tamaño del background.
    Se ha arreglado un problema de borrosidad en el enemigo.

--- Assets/Scripts/Camera/Camera Controller.cs
+++ Assets/Scripts/Camera/Camera Controller.cs
@@ -5,15 +5,9 @@ public class CameraController : MonoBehaviour
 
     public Transform target;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z); //Seguimiento de camara a personaje
     }
 }
--- Assets/Scripts/Enemies/Orc Warrior/Orc Warrior Movement.cs
+++ Assets/Scripts/Enemies/Orc Warrior/Orc Warrior Movement.cs
@@ -1,30 +1,38 @@
-
 using UnityEngine;
 
 public class OrcWarriorMovement : MonoBehaviour
 {
     public Transform jugador;
-    public float velocidad;
+    public float velocidad = 2f;
 
     private bool estaMirandoDerecha = true;
 
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = Vector2.MoveTowards(transform.position, jugador.position, velocidad * Time.deltaTime);
-
-        bool estaJugadorDerecha = transform.position.x < jugador.transform.position.x;
-        Flip(estaJugadorDerecha);
+        IASeguimiento();
     }
 
-    private void Flip(bool estaJugadorDerecha) 
+    void Update()
     {
-        if ((estaMirandoDerecha && !estaJugadorDerecha) || (!estaMirandoDerecha && estaJugadorDerecha)) 
+
+        bool estaJugadorDerecha = transform.position.x < jugador.transform.position.x; //Obtiene si esta el jugador en el lado derecho
+        Girar(estaJugadorDerecha);
+    }
+
+    private void IASeguimiento()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, jugador.position, velocidad * Time.deltaTime); //Seguimiento al jugador
+    }
+
+    private void Girar(bool estaJugadorDerecha)
+    {
+        //Gira el enemigo  segun la posicion del jugador
+        if ((estaMirandoDerecha && !estaJugadorDerecha) || (!estaMirandoDerecha && estaJugadorDerecha))
         {
             estaMirandoDerecha = !estaMirandoDerecha;
             Vector3 scale = transform.localScale;
             scale.x *= -1;
             transform.localScale = scale;
-
         }
     }
 }
